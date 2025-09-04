feef5136dd454ebb30f98f29a848b91b65f4dc54
itsfaisalhossen <itsfaisalhossen@gmail.com>
2025-09-05 01:06:16 +0600

    Added Speech Synthesis Utterance

--- script/index.js
+++ script/index.js
@@ -3,6 +3,12 @@ const createEl = (arr) => {
   return htmlEl.join(" ");
 };
 
+function pronounceWord(word) {
+  const utterance = new SpeechSynthesisUtterance(word);
+  utterance.lang = "en-EN"; // English
+  window.speechSynthesis.speak(utterance);
+}
+
 const manageSpinner = (status) => {
   if (status == true) {
     document.getElementById("spinner").classList.remove("hidden");
@@ -46,7 +52,6 @@ const loadWordDetails = async (id) => {
 };
 
 const displayWordDetails = (word) => {
-  console.log(word);
   const detailsContainerEl = document.getElementById("details-container");
   detailsContainerEl.innerHTML = `
            <div class="">
@@ -111,7 +116,9 @@ const displayLevelWord = (words) => {
                    })" class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]">
                       <i class="fa-solid fa-circle-info"></i>
                     </button>
-                    <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]">
+                    <button onclick="pronounceWord('${
+                      word.word
+                    }')" class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]">
                      <i class="fa-solid fa-volume-high"></i>
                     </button>
                  </div>
